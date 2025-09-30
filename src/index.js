11b01b2485e630c326281aeb377fac61f1bfa6cd
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-09-30 07:34:30 +0530

    000

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import config from './config.js';
 
 const app = express();
 
+// Serve SEB config at root path
 app.get('/', (req, res) => {
   // Set proper Content-Type and headers for SEB config
   res.set('Content-Type', 'application/x-plist');
+  res.set('Content-Disposition', 'attachment; filename="config.seb"');
+  res.send(config);
+});
+
+// Add this new route to serve the same content at /config.seb path
+app.get('/config.seb', (req, res) => {
+  res.set('Content-Type', 'application/x-plist');
+  res.set('Content-Disposition', 'attachment; filename="config.seb"');
   res.send(config);
 });
 
